ed37a33022aad18f02451736b529d887c83d5441
465837@niuitmo.ru <465837@niuitmo.ru>
2025-12-11 15:29:00 +0300

    formatting

--- app/manager.py
+++ app/manager.py
@@ -1,45 +1,36 @@
-from openai import AsyncOpenAI
-from httpx import AsyncClient
-
-from config import MLP_API_KEY, DEFAULT_MODERATOR, PROMPT, PROVIDER_URL, MODELS_DICT
-from schemas import Rule, ResponseWithReasoning, ResumeToGovernment, DEFAULT_RULES
-from resume_text_converter import resume_to_text
-
-from typing import List
 import json
 import re
+from typing import List
 
+from config import DEFAULT_MODERATOR, MLP_API_KEY, MODELS_DICT, PROMPT, PROVIDER_URL
+from httpx import AsyncClient
+from openai import AsyncOpenAI
+from resume_text_converter import resume_to_text
+from schemas import DEFAULT_RULES, ResponseWithReasoning, ResumeToGovernment, Rule
 
-client = AsyncOpenAI(
-    api_key=MLP_API_KEY,
-    base_url=PROVIDER_URL,
-    http_client=AsyncClient(
-        proxy=None,
-        timeout=600
-    )
-)
+client = AsyncOpenAI(api_key=MLP_API_KEY, base_url=PROVIDER_URL, http_client=AsyncClient(proxy=None, timeout=600))
 
 
 def parse_answer(response_str: str) -> ResponseWithReasoning:
     parts = response_str.split("Результат:")
-    
+
     reasoning = parts[0].replace("Рассуждения:", "").strip()
 
-    json_match = re.search(r'```json\n(.*?)\n```', parts[1], re.DOTALL)
-    json_str = json_match.group(1) if json_match else '{}'
+    json_match = re.search(r"```json\n(.*?)\n```", parts[1], re.DOTALL)
+    json_str = json_match.group(1) if json_match else "{}"
 
     try:
         result_dict = json.loads(json_str)
     except json.JSONDecodeError:
         result_dict = {"error": "Invalid JSON format"}
-    
+
     return ResponseWithReasoning.model_validate({"reasoning": reasoning, "result": result_dict})
 
 
-async def moderate(resume: ResumeToGovernment, rules: List[Rule]=None, moderator_model: str=None) -> ResponseWithReasoning:
+async def moderate(resume: ResumeToGovernment, rules: List[Rule] = None, moderator_model: str = None) -> ResponseWithReasoning:
     if rules is None:
         rules = DEFAULT_RULES
-    
+
     if moderator_model is None:
         provider_model_name = MODELS_DICT[DEFAULT_MODERATOR]
     else:
@@ -47,18 +38,15 @@ async def moderate(resume: ResumeToGovernment, rules: List[Rule]=None, moderator
 
     resume_text = resume_to_text(resume=resume)
 
-    formatted_prompt = PROMPT.replace('{rules}', '\n'.join([rule.model_dump_json() for rule in rules])).replace('{resume_text}', resume_text)
+    formatted_prompt = PROMPT.replace("{rules}", "\n".join([rule.model_dump_json() for rule in rules])).replace(
+        "{resume_text}", resume_text
+    )
 
     completion = await client.chat.completions.create(
-        messages=[
-            {
-                "role": "user",
-                "content": formatted_prompt
-            }
-        ],
+        messages=[{"role": "user", "content": formatted_prompt}],
         model=provider_model_name,
     )
-    
+
     answer_content = completion.choices[0].message.content
 
     return parse_answer(answer_content)
--- app/routers.py
+++ app/routers.py
@@ -1,12 +1,10 @@
-from fastapi import APIRouter
-
-from schemas import SelectionContext, FinalResponse
-from manager import moderate
-
 import time
 
-router = APIRouter(prefix='/moderator',
-                   tags=['Moderator'])
+from fastapi import APIRouter
+from manager import moderate
+from schemas import FinalResponse, SelectionContext
+
+router = APIRouter(prefix="/moderator", tags=["Moderator"])
 
 
 @router.post("/reserve/selection")
@@ -14,16 +12,10 @@ async def reserve_selection(selection_context: SelectionContext) -> FinalRespons
     start_time = time.perf_counter()
 
     moderation_result = await moderate(
-        resume=selection_context.resume,
-        rules=selection_context.rules,
-        moderator_model=selection_context.moderation_model
+        resume=selection_context.resume, rules=selection_context.rules, moderator_model=selection_context.moderation_model
     )
 
     end_time = time.perf_counter()
     time_ms = int((end_time - start_time) * 1000)
 
-    return FinalResponse(
-        reasoning=moderation_result.reasoning,
-        result=moderation_result.result,
-        time_ms=time_ms
-    )
+    return FinalResponse(reasoning=moderation_result.reasoning, result=moderation_result.result, time_ms=time_ms)
